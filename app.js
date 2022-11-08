2575993792c8541f2cfe4aa0c4606e0fd8fba3f3
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-11-08 15:48:56 +0100

    implement function to start and show passing time

---
+++ app.js
@@ -0,0 +1,33 @@
+const startBtn = document.querySelector(".play");
+const pauseBtn = document.querySelector(".pause");
+const stopBtn = document.querySelector(".stop");
+const resetBtn = document.querySelector(".reset");
+const historyBtn = document.querySelector(".history");
+const stopwatch = document.querySelector(".stopwatch");
+const time = document.querySelector(".time");
+const timeList = document.querySelector(".time-list");
+
+const infoBtn = document.querySelector(".info");
+const modalShadow = document.querySelector("modal-shadow");
+const closeModalBtn = document.querySelector(".close");
+
+let countTime;
+let minutes = 0;
+let seconds = 0;
+const handleStart = () => {
+  countTime = setInterval(() => {
+    if (seconds < 9) {
+      seconds++;
+      stopwatch.textContent = `${minutes}:0${seconds}`;
+    } else if (seconds >= 9 && seconds < 59) {
+      seconds++;
+      stopwatch.textContent = `${minutes}:${seconds}`;
+    } else {
+      minutes++;
+      seconds = 0;
+      stopwatch.textContent = `${minutes}:00`;
+    }
+  }, 1);
+};
+
+startBtn.addEventListener("click", handleStart);
